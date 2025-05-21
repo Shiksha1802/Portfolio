58bb13d85c2af0cc2f94e93b4995bedff0d93be9
Shiksha Rawat <shiksharawat1802@gmail.com>
2025-05-21 18:23:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Message sent! (This is just a demo.)");
+});
